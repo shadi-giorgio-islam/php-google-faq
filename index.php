3363ea7d80299ef124a2d4fb69ff9c12e5fbd0b0
Giorgio <shadigiorgioislam@gmail.com>
2021-02-26 16:55:45 +0100

    Add lists and END

--- index.php
+++ index.php
@@ -24,7 +24,22 @@
     ],
     [
       'question' => 'Perché il mio account è associato a un paese?',
-      'answer' => 'Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.'
+      'answer' => '
+      Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+      <br>
+      <br>
+      <ol>
+        <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+        <ol type="a">
+          <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+          <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+        </ol>
+        </li>
+        <br>
+        <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
+      </ol>
+      <br>
+      Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.'
     ],
     [
       'question' => 'Stabilire il paese associato al tuo account',
