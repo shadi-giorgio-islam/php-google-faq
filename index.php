c5504517876ba6cfdba05f016dc0d2f1b06122c4
Giorgio <shadigiorgioislam@gmail.com>
2021-02-26 15:45:11 +0100

    Add header

--- index.php
+++ index.php
@@ -6,9 +6,37 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="css/style.css">
-  <title>Domande frequenti</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+  <title>Domande frequenti-Privacy e termini</title>
 </head>
 <body>
-
+  <!-- inzio header -->
+  <header>
+    <!-- inizio menu -->
+    <nav>
+      <!-- inizio menu-top -->
+      <div class="menu-top">
+        <ul>
+          <li><a href="#"><img src="https://loghi-famosi.com/wp-content/uploads/2020/09/Google-Logo-650x366.png" alt="logo"></a></li>
+          <li><a href="#">Privacy e termini</a></li>
+        </ul>
+      </div>
+      <!-- fine menu-top -->
+      <!-- inizio menu-bottom -->
+      <div class="menu-bottom">
+        <ul>
+          <li><a href="#">Introduzione</a></li>
+          <li><a href="#">Norme sulla privacy</a></li>
+          <li><a href="#">Termini di servizio</a></li>
+          <li><a href="#">Tecnologie</a></li>
+          <li><a href="#">Domande frequenti</a></li>
+        </ul>
+      </div>
+      <!-- fine menu-bottom -->
+    </nav>
+    <!-- fine menu -->
+  </header>
+  <!-- fine header -->
 </body>
 </html>
